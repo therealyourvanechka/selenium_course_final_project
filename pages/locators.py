b45c5d5e2151b86d000ea0ed8a0e751942ef1636
Ivan <116534972+therealyourvanechka@users.noreply.github.com>
2026-04-23 21:18:05 +0500

    Remove dublicate LoginPageLocators class from locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -7,10 +7,6 @@ class BasePageLocators():
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn-default")
 
-class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
